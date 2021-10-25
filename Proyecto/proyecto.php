e5c420ca110d9c3592231dbb66737362096d2588
AlvaroFernandezRodriguez <44578868+AlvaroFernandezRodriguez@users.noreply.github.com>
2021-10-25 22:51:46 +0200

    commit casa
    commit casa

--- Proyecto/proyecto.php
+++ Proyecto/proyecto.php
@@ -1,3 +1,11 @@
 <?php
-    
+echo '<link rel="stylesheet" href="estilos.css" type="text/css">';
+  $fichero= fopen("D:\\xampp\\apache\\logs\\error.log", "r");
+ echo "<textarea>";
+ while (!feof($fichero)){
+     $linea= fgets($fichero);
+     echo $linea;
+ } 
+ fclose($fichero);
+ echo "</textarea>"; 
 ?>
